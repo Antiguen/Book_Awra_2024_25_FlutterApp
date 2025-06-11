4ef80583375bdbfd28430b715e1de8b998f4f476
selamawit shimeles <162134523+selamawitsh@users.noreply.github.com>
2025-06-11 18:04:51 +0300

    Add files via upload

---
+++ bookaurabackend/dist/schemas/user.schema.d.ts
@@ -0,0 +1,28 @@
+import { Document } from "mongoose";
+import { UserRole } from "./user-role.enum";
+export declare class User extends Document {
+    userId: string;
+    fullName: string;
+    email: string;
+    userPassword: string;
+    dateOfBirth: string;
+    gender: string;
+    role: UserRole;
+    status: boolean;
+    verificationCode: string;
+    library: string[];
+    bio: string;
+    genre: string;
+    description: string;
+    imageData: Buffer;
+    imageContentType: string;
+}
+export declare const UserSchema: import("mongoose").Schema<User, import("mongoose").Model<User, any, any, any, Document<unknown, any, User, any> & User & Required<{
+    _id: unknown;
+}> & {
+    __v: number;
+}, any>, {}, {}, {}, {}, import("mongoose").DefaultSchemaOptions, User, Document<unknown, {}, import("mongoose").FlatRecord<User>, {}> & import("mongoose").FlatRecord<User> & Required<{
+    _id: unknown;
+}> & {
+    __v: number;
+}>;
